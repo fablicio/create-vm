cd0e334698ae6367f2979bd763f646126cb3ca96
Fablicio Soares <fablicio.soares@gmail.com>
2024-06-24 02:29:52 +0000

    ajuste-code

--- VMware/hom/main.tf
+++ VMware/hom/main.tf
@@ -3,13 +3,7 @@ locals {
   password             = "^kv6TUIYQ3@BpLqJ"
   vsphere_server       = "192.168.0.5"
   allow_unverified_ssl = true
- # location = "eastus"
- # locationname = "eus"
- # ambiente = "hom"
- # vnetname = "vnet-eastus-hom-inloco"
- # rgvnet = "rg-eastus-hom-inloco-network"
- # sistema = "Inloco"
-#}
+ }
 
 module "hom-dc" {
   source = "../modulos/datacenter"
